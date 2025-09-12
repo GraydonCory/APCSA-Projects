a11b90acc7360c1a82fc45130ee534584393497f
GraydonCory <119955137+GraydonCory@users.noreply.github.com>
2025-09-12 14:19:59 +0000

    Setup the book analytics project

---
+++ BookAnalyticsProject/GraydonBookAnalytics.java
@@ -0,0 +1,98 @@
+public class GraydonBookAnalytics extends BookAnalytics{
+    @Override
+    public String mostFrequentWord() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public String mostFrequentWordWithCapitalFirstLetter() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public char mostFrequentLetter() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+    
+    @Override
+    public int wordCount() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public int characterCount() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public int vowelCount() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public int consonantCount() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public double averageWordLength() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public int sentenceCount() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public double averageWordsPerSentance() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public int numberOfDifferentWords() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public String alphabetizeAllByChar() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public String alphabetizeAllByWord() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public String replaceWordAndPreserveCase(String aLine, String wordOne, String wordTwo) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public String toString() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void replaceWordsAndWriteToFile(String[] wordsToBeReplaced, String[] wordsToReplaceWith) {
+        // TODO Auto-generated method stub
+        
+    }
+    
+}
